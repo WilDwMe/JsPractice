b8cf31a9f2e08ed996217b89b01e10da68b84831
WilD <a.nazarenko@nylonfabric.ru>
2021-02-02 12:42:24 +0300

    add some more tabs

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -12,13 +12,15 @@ const modals = () => {
                     if(e.target) {
                         e.preventDefault();
                     }
-
+            modal.classList.add('animated');
+            modal.classList.add('fadeIn');
             modal.style.display = 'block';
             document.body.classList.add('modal-open');
         });
     });
 
         close.addEventListener('click', () => {
+
             modal.style.display = 'none';
             document.body.classList.remove('modal-open');
             // document.body.style.overflow = '';          
@@ -34,8 +36,18 @@ const modals = () => {
     }
 
 
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = "block";
+              document.body.style.overflow = 'hidden';  
+        }, time);
+    }
+
     bindModal('.popup_engineer_btn','.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup_close');
+
+
+    // showModalByTime('.popup', 60000);
 
 
 
